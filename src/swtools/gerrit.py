b4ef30785a4ce7f92a61ad51a2b7b2a00157f07c
Sunjoo Park <allessunjoo.park@lge.com>
2018-01-26 10:49:58 +0900

    Gerrit: Add exception cases for not having owner information
    Description:
    By default,  "/projects/{project-name}/access" API's return value have
    "owner_of" field. This field has a list of refspecs defined in an access
    configuration page. If one refspec doesn't have 'Owner' permission
    there, it doesn't matter.
    
    ["local"][{refspec}}["permissions"] has all detailed permissions defined.
    But previous code didn't consider it. So KeyError has been occured when
    calling ["local"][{refspec}}["permissions"]["owner"]["rules"].
    
    Solution:
    Add try/except code to handle KeyError

--- src/swtools/gerrit.py
+++ src/swtools/gerrit.py
@@ -53,6 +53,8 @@ class Gerrit:
         return grp_info
 
     def get_project_owners(self, prj_name):
+        # API Link: https://gerrit-documentation.storage.googleapis.com/Documentation/2.13.9/rest-api-projects.html#get-access
+        # API URI: 'GET /projects/{project-name}/access'
         owners_info = {}
         prj_name_quoted = urllib.parse.quote(prj_name, safe="")
         prj_access_url = self.api_prj_url + prj_name_quoted + "/access"
@@ -64,10 +66,20 @@ class Gerrit:
             logger.warning(prj_access_req.status_code)
             logger.warning(prj_access_req.text)
             return
+        # "owner_of"
+        #  - This field has all refspec defined in project access configuration
+        #    regardless of having 'owner' permission configuration.
+        #  - If there is no access configuration on web UI, "owner_of" only has "refs/*" value
         owner_of = prj_access_info["owner_of"]
+        logger.debug("Owner of: {}".format(owner_of))
         for owner_refspec in owner_of:
+            try:
+                # Check if having 'owner' permission or not
+                owners = prj_access_info["local"][owner_refspec]["permissions"]["owner"]["rules"]
+            except KeyError:
+                logger.error("There is no owner information in {}' refspec {}".format(prj_name, owner_refspec))
+                continue
             owners_info[owner_refspec] = []
-            owners = prj_access_info["local"][owner_refspec]["permissions"]["owner"]["rules"]
             for owner_id in owners:
                 owner_info = self.get_group_info(owner_id)
                 owners_info[owner_refspec].append(owner_info)
@@ -75,14 +87,10 @@ class Gerrit:
 
 
 if __name__ == "__main__":
-    # gerrit_url = input("Gerrit Url: ")
-    # user_account = input("User account: ")
-    # user_password = input("User Password: ")
+    gerrit_url = input("Gerrit Url: ")
+    user_account = input("User account: ")
+    user_password = input("User Password: ")
     project_name = input("Project name: ")
-    g = Gerrit("http://127.0.0.1:8080/", "all4dich", "4XRFQUmsI1WKVGS6/t7vTUi/z5BPXpT+FSzpv8aGDg")
+    g = Gerrit(gerrit_url, user_account, user_password)
     owners = g.get_project_owners(project_name)
     logger.warning("\n{}".format(owners))
-
-    g2 = Gerrit("http://127.0.0.1:8090/", "all4dich", "W8dQBjiStsEzzYVtrwKjOI8RkrFDSRaiQB7p9fxjbA")
-    owners2 = g2.get_project_owners(project_name)
-    logger.warning("\n{}".format(owners2))
